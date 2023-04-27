37b180d48a7b49ea22761e310b622086f6002dc7
kydbri <prncsekyd@gmail.com>
2023-04-26 17:36:41 -0700

    Revert "Merge branch 'main' of https://github.com/RESIDGE/mancala-game"
    This reverts commit aa22a9312956f03cb9723ca075f230bb4301b066, reversing
    changes made to 477505877add614daf314634807b177b4379da21.

--- Mancala/src/Pit.java
+++ Mancala/src/Pit.java
@@ -5,85 +5,111 @@
  */
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.geom.*;
 
 /**
  * 
  */
 public class Pit extends JPanel{
-
-	private static final int STONE_SIZE = 10;
 	
-	private String pitName;
-	private boolean pitOwner;
+	//Constant Variables
+	private final int PIT_BORDER_HEIGHT = 70;	
+	private final int PIT_BORDER_WIDTH = 60;
+	private final int STONE_SIZE = 5;
+	
+	//Private Instance Variables
 	private int pitID;
 	private int stoneAmt;
+	private boolean pitOwner; //True = Player A, False = Player B
+	private String pitName;
 	
-	public Pit(String pitName, boolean pitOwner, int pitID, int width, int height) {
-		setLayout(new BorderLayout());
-		stoneAmt = 10;
+	/**
+	 * 
+	 * @param pitName the pit name
+	 * @param pitOwner true if pit belongs to Player A; otherwise, 
+	 * @param pitID
+	 */
+	public Pit(String pitName, boolean pitOwner, int pitID) {
+		this.pitName = pitName;
+		this.pitOwner = pitOwner;
+		this.pitID = pitID;		
+		stoneAmt = 7;
 		
-		JLabel pitLabel = new JLabel(pitName, SwingConstants.CENTER);
+		setLayout(new BorderLayout());
+	
+		//Pit Name
 		if(pitOwner) {
-			add(pitLabel, BorderLayout.SOUTH);
+			add(new JLabel(pitName, JLabel.CENTER), BorderLayout.SOUTH);
 		}
 		else {
-			add(pitLabel, BorderLayout.NORTH);
+			add(new JLabel(pitName, JLabel.CENTER), BorderLayout.NORTH);
 		}
 		
-		drawPit(this, width, height);
-	}
-
-	//METHODS
-	public static void drawPit(Pit pitFrame, int width, int height) {
-		JPanel pitStorage = new JPanel() {		
+		//Pit Storage
+		JPanel pitStorage = new JPanel() {
+			@Override
+			public void setBorder(Border border) {
+				super.setBorder(new EmptyBorder(15, 0, 0, 0));
+			}
+		
 			@Override
 			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
 				Graphics2D g2 = (Graphics2D) g;
-				Ellipse2D.Double pitBorder = new Ellipse2D.Double(0, 0, width, height);
+				Ellipse2D.Double pitBorder = new Ellipse2D.Double(0, 0, PIT_BORDER_WIDTH, PIT_BORDER_HEIGHT);
 				g2.draw(pitBorder);
 			}
+			
+			@Override
+			public Dimension getPreferredSize() {
+				return new Dimension(PIT_BORDER_WIDTH, PIT_BORDER_HEIGHT);
+			}
 		};
-		pitStorage.setLayout(new BorderLayout());
-		pitStorage.setPreferredSize(new Dimension(width, height));
-		pitStorage.setBorder(BorderFactory.createEmptyBorder(width / 4, height / 10, width / 4, height / 10));
-		pitFrame.add(pitStorage, BorderLayout.CENTER);
 		
-		JPanel stoneStorage = new JPanel();
-		stoneStorage.setLayout(new FlowLayout());
-		stoneStorage.setPreferredSize(new Dimension(width, height));
-		
-		for(int i = 0; i < pitFrame.getStoneAmt(); i ++) {
-			drawStone(stoneStorage);
+		JPanel stoneArea = new JPanel() {
+			@Override
+			public Dimension getPreferredSize() {
+				return new Dimension(50, 35);
+			}
+		};
+		stoneArea.setLayout(new FlowLayout(FlowLayout.CENTER));
+		for(int i = 0; i < stoneAmt; i++) {
+			addStones(stoneArea);
 		}
-		pitStorage.add(stoneStorage, BorderLayout.CENTER);
+		
+		pitStorage.add(stoneArea, BorderLayout.CENTER);
+		
+		add(pitStorage, BorderLayout.CENTER);
 	}
 	
-	public static void drawStone(JPanel pitStorage) {
+	//METHODS
+	public void addStones(JPanel stoneArea) {
 		Icon stoneIcon = new Icon() {
 			@Override
 			public void paintIcon(Component c, Graphics g, int x, int y) {
 				Graphics2D g2 = (Graphics2D) g;
-				Ellipse2D.Double stoneShape = new Ellipse2D.Double(0, 0, STONE_SIZE, STONE_SIZE);
+				Ellipse2D.Double stone = new Ellipse2D.Double(x, y, STONE_SIZE, STONE_SIZE);
 				g2.setColor(Color.BLACK);
-				g2.draw(stoneShape);
-				g2.fill(stoneShape);
+				g2.fill(stone);
 			}
 
 			@Override
 			public int getIconWidth() {
+				// TODO Auto-generated method stub
 				return STONE_SIZE;
 			}
 
 			@Override
 			public int getIconHeight() {
+				// TODO Auto-generated method stub
 				return STONE_SIZE;
 			}
 			
 		};
 		
 		JLabel stone = new JLabel(stoneIcon);
-		pitStorage.add(stone);
+		stoneArea.add(stone);
 	}
 	
 	//GETTERS
@@ -126,7 +152,7 @@ public class Pit extends JPanel{
 		
 		frame.setLayout(new FlowLayout());
 		
-		Pit pit = new Pit("1A", true, 0, 80, 100);
+		Pit pit = new Pit("1A", true, 0);
 		frame.add(pit);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
